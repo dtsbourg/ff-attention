8681eea72d4ce7bd426157c07d2bd30d1589d7d8
dtsbourg <dtsbourg@gmail.com>
2018-06-08 13:34:43 +0200

    init

---
+++ ff_attention.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# ff-attention in Pytorch package, created by
+# Dylan Bourgeois (@dtsbourg)
+#
+# License: MIT (see LICENSE.md)
+#
+# Original work by Colin Raffel and Daniel P. Ellis
+# "Feed-Forward Networks with Attention Can
+# Solve Some Long-Term Memory Problems"
+# https://arxiv.org/abs/1512.08756
+# (Licensed under CC-BY)
