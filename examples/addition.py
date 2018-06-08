00377b17d0f91764770dd475968f80b385540c5f
dtsbourg <dtsbourg@gmail.com>
2018-06-08 15:10:53 +0200

    license

--- examples/addition.py
+++ examples/addition.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# ff-attention in Pytorch package, created by
+# Dylan Bourgeois (@dtsbourg)
+#
+# License: MIT (see LICENSE.md)
+#
+# Original work by Colin Raffel and Daniel P. Ellis
+# "Feed-Forward Networks with Attention Can
+# Solve Some Long-Term Memory Problems"
+# https://arxiv.org/abs/1512.08756
+# (Licensed under CC-BY)
+
 import torch
 import torch.nn.functional as F
 from torch.utils.data.dataset import Dataset
